4b95beb7a0b81e8c09d9bd5b3587985c9212d98e
Vibhor <94433124+vibzz2003@users.noreply.github.com>
2024-04-20 12:03:47 +0530

    Time: 266 ms (23.77%) | Memory: 25.7 MB (28.35%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #[1,2,3,4] -> pointer 1 pe ayega -> 1 chodh ke baaki sabko multiply karna hoga-> 1 ko ans se replace karna hoga
+        # hum jabh koi element par aaye -> uss element ko 1 karde -> sabka mul le -> element ki jagah daal de
+        size = len(nums)
+        ans = [1]*size
+        left = 1
+        right = 1
+        for i in range(size):
+            ans[i] *= left
+            left *= nums[i]
+
+        for i in range(size-1, -1, -1):
+            ans[i] *= right
+            right *= nums[i]
+
+        return ans
+
+        #[1,2,3,4]
+        #size = 4
+        #ans = 12
+        #nums[1] =12
+        #j = 0
+        #
