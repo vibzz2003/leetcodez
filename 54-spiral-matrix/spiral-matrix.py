4bee082d206902734a271f977f0de1c2159de7c0
Vibhor <94433124+vibzz2003@users.noreply.github.com>
2024-06-25 22:28:13 +0530

    Time: 41 ms (18.21%) | Memory: 16.5 MB (42.43%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+
+        row_begin = 0
+        row_end = len(matrix)
+        col_begin = 0
+        col_end = len(matrix[0])
+        res = []
+
+        while row_end>row_begin and col_end>col_begin:
+            for i in range(col_begin, col_end):
+                res.append(matrix[row_begin][i])
+            for j in range(row_begin + 1, row_end - 1):
+                res.append(matrix[j][col_end-1])
+            if row_end != row_begin+1:
+                for i in range(col_end-1, col_begin-1, -1):
+                    res.append(matrix[row_end-1][i])
+            if col_begin != col_end-1:
+                for j in range(row_end-2, row_begin, -1):
+                    res.append(matrix[j][col_begin])
+            row_begin+=1
+            row_end-=1
+            col_begin+=1
+            col_end-=1
+
+        return res
+
+        #for i in range (1st column to 3rd column)
+        #append matrix[0][0], m[0][1], m[0][2]
+        #for j in range (2nd row to last row):
+        #append m[1][2], m[2][2] col_end-1 as col_end = len which is 3 and indexing starts from 0
+        #now row_end(2)!=row(begin+1)(1)
+        #for i in range (1st column to -1 column):
+        #append m[2][1] amd m[2][0]
+        #for i in range (row 1 to 0 in decreasing order):
+        #append m[1][0]
+        #then update the value of row_begin to 1, row_end to 2, col_begin to 1 and col_end to 2
+        #and then the loop again
