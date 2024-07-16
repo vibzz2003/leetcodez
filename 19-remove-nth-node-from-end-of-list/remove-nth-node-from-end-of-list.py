8359992fe03587da0fe11a39774269016be4822b
Vibhor <94433124+vibzz2003@users.noreply.github.com>
2024-07-16 22:56:25 +0530

    Time: 39 ms (41.37%) | Memory: 16.4 MB (94.96%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -8,31 +8,51 @@ class Solution:
         #1->2->3->4->5 = stack(5,4,3,2,1) = list[5,4,3,2,1]/5->4->3->2->1 = reverse again
         # or
         #1->2->3->4->5 = list[1,2,3,4,5] = reverse[list] = list[5,4,3,2,1] = operation
-        if n == 0 or head is None:
+        # if n == 0 or head is None:
+        #     return head
+
+        # list1 = []
+
+        # curr = head
+        # while curr:
+        #     list1.append(curr.val)
+        #     curr = curr.next
+
+        # #reverse
+        # reversedlist = list1[::-1]
+
+        # if n <= len(reversedlist):
+        #     reversedlist.pop(n-1)
+
+        # list2 = reversedlist[::-1]
+
+        # if not list2:
+        #     return None
+
+        # dummy = ListNode(0)
+        # curr = dummy
+        # for val in list2:
+        #     curr.next = ListNode(val)
+        #     curr = curr.next
+        
+        # return dummy.next
+
+        if not head or n == 0:
             return head
 
-        list1 = []
-
-        curr = head
-        while curr:
-            list1.append(curr.val)
-            curr = curr.next
-
-        #reverse
-        reversedlist = list1[::-1]
-
-        if n <= len(reversedlist):
-            reversedlist.pop(n-1)
-
-        list2 = reversedlist[::-1]
-
-        if not list2:
-            return None
-
         dummy = ListNode(0)
-        curr = dummy
-        for val in list2:
-            curr.next = ListNode(val)
-            curr = curr.next
-        
+        dummy.next = head
+
+        first = dummy
+        second = dummy
+
+        for i in range(n+1):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+
         return dummy.next
