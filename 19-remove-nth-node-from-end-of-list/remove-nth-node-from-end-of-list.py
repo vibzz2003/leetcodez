0865c24928c01b3ad91206acee4b5453ee7bbbda
Vibhor <94433124+vibzz2003@users.noreply.github.com>
2024-07-16 22:52:57 +0530

    Time: 44 ms (10.41%) | Memory: 16.5 MB (64.75%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #1->2->3->4->5 = stack(5,4,3,2,1) = list[5,4,3,2,1]/5->4->3->2->1 = reverse again
+        # or
+        #1->2->3->4->5 = list[1,2,3,4,5] = reverse[list] = list[5,4,3,2,1] = operation
+        if n == 0 or head is None:
+            return head
+
+        list1 = []
+
+        curr = head
+        while curr:
+            list1.append(curr.val)
+            curr = curr.next
+
+        #reverse
+        reversedlist = list1[::-1]
+
+        if n <= len(reversedlist):
+            reversedlist.pop(n-1)
+
+        list2 = reversedlist[::-1]
+
+        if not list2:
+            return None
+
+        dummy = ListNode(0)
+        curr = dummy
+        for val in list2:
+            curr.next = ListNode(val)
+            curr = curr.next
+        
+        return dummy.next
