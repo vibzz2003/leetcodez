414b629b0ca04ace7057de441987d9d2bd565ec4
Vibhor <94433124+vibzz2003@users.noreply.github.com>
2024-07-23 22:33:29 +0530

    Time: 1052 ms (80.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+(select name as results
+from MovieRating join Users using (user_id)
+group by name
+order by count(*) desc, name
+limit 1)
+
+union all
+
+(select title as results
+from MovieRating join Movies using(movie_id)
+where extract(YEAR_MONTH FROM created_at) = 202002
+group by title
+order by avg(rating) desc, title
+limit 1)
